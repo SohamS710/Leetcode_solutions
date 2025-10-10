8f7bb189b1e71d494ac01d61fc73f68cdfdf6538
SohamS710 <sohamshinde07102@gmail.com>
2025-10-10 22:32:48 +0530

    Time: 497 ms (23.79%), Space: 329.1 MB (33.09%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.cpp
@@ -1,28 +1,33 @@
 class Solution {
 public:
-    bool validPath(int n, vector<vector<int>>& edges, int start, int end) {
-        unordered_map<int,vector<int>> graph; 
-        for(auto e : edges) {
-            graph[e[0]].push_back(e[1]);
-            graph[e[1]].push_back(e[0]);
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        unordered_map <int, vector<int>> mp;
+        for (vector<int> &edge : edges) {
+            int u = edge[0];
+            int v= edge[1];
+
+            mp[u].push_back(v);
+            mp[v].push_back(u);
         }
-        vector<bool> visited(n,0);        
-        queue<int> q;
-        q.push(start);
-        visited[start] = 1; 
-        while(!q.empty()) {
-            int curr = q.front();
-            q.pop();
-            if(curr == end)
-                return 1; 
-            for(auto &node : graph[curr]){
-                if(!visited[node]){
-                    visited[node] = 1; 
-                    q.push(node);
+
+        vector <bool> visited(n,false);
+        
+        queue<int> que;
+        que.push(source);
+        visited[source] = true;
+        while (!que.empty()) {
+            int node = que.front();
+            que.pop();
+
+            if (node == destination) return true;
+
+            for (auto &V : mp[node]) {
+                if (!visited[V]) {
+                    que.push(V);
+                    visited[V] = true;
                 }
             }
         }
-        
         return false;
     }
 };
