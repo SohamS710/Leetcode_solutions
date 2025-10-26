cd2a30e26277dadca5d93faa5440d56d682f958a
SohamS710 <sohamshinde07102@gmail.com>
2025-10-26 13:03:12 +0530

    Time: 0 ms (100%), Space: 23.9 MB (20.6%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int left = 0; // position to place the next non-zero
-
-        // Move all non-zero numbers to the front
+        int left = 0; // where next non-zero should go
         for (int right = 0; right < nums.size(); right++) {
             if (nums[right] != 0) {
-                nums[left] = nums[right];
-                if (left != right) nums[right] = 0; // set the old position to 0
+                swap(nums[left], nums[right]);
                 left++;
             }
         }
